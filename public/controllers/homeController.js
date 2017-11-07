8b693a90173dd43fa47b482064c4e160db118a20
ZarinaValeva <zarina.valentinova@gmail.com>
2017-11-07 13:51:24 +0200

    7bit to base64 not working

--- public/controllers/homeController.js
+++ public/controllers/homeController.js
@@ -67,12 +67,18 @@ appNG.controller('homeController', function ($scope, $http, $location) {
     var firstPic = reversePics[0];
     console.log(firstPic);
     var user = JSON.parse(sessionStorage.getItem('user'));
-    user.url = firstPic.address;
+    // user.url = firstPic.address;
     var base64 = firstPic.address;
     // var buf = new Buffer(base64, 'base64'); // Ta-da
-    console.log(buf);
-    console.log(user.url)
+    // console.log(buf);
+    console.log(base64)
     sessionStorage.setItem('user', JSON.stringify(user));
+    var snimka = function hexToBase64(str) {
+        return btoa(String.fromCharCode.apply(null, str.replace(/\r|\n/g, "").replace(/([\da-fA-F]{2}) ?/g, "0x$1 ").replace(/ +$/, "").split(" ")));
+    }
+    var lastUrl = snimka(base64);
+    user.url = 'data:image/jpeg;base64,' + lastUrl;
+    console.log(user.url)
     
     
     })
