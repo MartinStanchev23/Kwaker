3106cdb10311b6e31bbcf3dbafee29a19ea293c3
ZarinaValeva <zarina.valentinova@gmail.com>
2017-11-07 22:27:43 +0200

    messages indexed

--- public/controllers/messageController.js
+++ public/controllers/messageController.js
@@ -1,20 +1,31 @@
 appNG.controller('message', function ($scope, $http, $location) {
     $scope.viewMessages = function () {
         var activeUserName = JSON.parse(sessionStorage.getItem('user')).username;
-        
+
         $http.get('/api/messages').then(function (data, status, headers, config) {
             var arr = data.data;
             console.log(arr);
-            var result = arr.filter(function(x){
-                if(x.usernameR == activeUserName){
+            var result = arr.filter(function (x) {
+                if (x.usernameR == activeUserName) {
                     return x;
                 }
             })
             console.log(result);
             $scope.result = result;
         })
-        $scope.sendReply = function(usernameS){
-            console.log(usernameS);
+        $scope.showReplyForm = false;
+        $scope.sendReply = function (user, index) {
+
+        }
+        var la;
+        $scope.showReply = function (index) {
+            la = index;
+            $scope.showReplyForm = false;
+        }
+        
+        $scope.hideReply = function (index) {
+            la = index;
+            $scope.showReplyForm = true;
         }
     }
 });
